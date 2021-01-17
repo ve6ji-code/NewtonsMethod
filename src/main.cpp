d7ac399fcddad047ed67f623c8260e126709c098
ve6ji-code <matt@ve6ji.ca>
2021-01-17 08:13:27 -0700

    Minor Edits

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,36 @@
 // ve6mjl@gmail.com
 // ----------------------------------------------------------------------------
 #include <iostream>
+
 namespace ve6ji
 {
+    // Use namespace to avoid confusion with other llibraries
     double sqrt(double x, double eps);
     double abs(double x);
 } // namespace ve6ji
+
 int main(int argc, char const *argv[])
 {
     std::cout.precision(12);
     // Value to find the square root of
-    double value = 18.333;  // Replace with your value
+    double value = 1877.333; // Replace with your value
     std::cout << "Calculating the square root of : " << value << std::endl;
     // Epsilon: Set your desired precision
     double epsilon = 0.000000001;
-    ve6ji::sqrt(value,epsilon);
+    ve6ji::sqrt(value, epsilon);
     return 0;
 }
 
 double ve6ji::sqrt(double x, double eps)
-{   std::cout.precision(24);
+{
+    std::cout.precision(24); // Show desired precision
     double result = 0.0;
-    double x_old = x - (x / 4.0); // Initial guess.  Play with this
+    double x_old = x - (x / 1.5); // Initial guess.  Play with this
     double x_new = 0.0;
     double diff = 0.0;
     int iter = 30; // Number of iterations to calculate
     int i = 0;
-    
+
     while (i < iter && x_old > 0.0)
     {
         x_new = 0.5 * (x_old + x / x_old);
@@ -51,6 +55,7 @@ double ve6ji::sqrt(double x, double eps)
 }
 double ve6ji::abs(double x)
 {
+    // Returns the absolute value
     if (x == 0 || x > 0)
     {
         return x;
